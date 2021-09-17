87666a2f946bfe1867a2a2c61d505a90f0a2a905
trees2brady <trees2brady@gmail.com>
2021-09-17 16:51:02 -0400

    ss

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-startTime = datetime.datetime.now()
-for i in range(100):
-    pass
-endTime = datetime.datetime.now()
-print ("index text corpus running time: ", endTime - startTime)
